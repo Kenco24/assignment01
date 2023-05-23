ed3024ce2bba29ed0d364845864b7b5e7a2cf41f
Kenan Sipahi <kenan.sipahi@hotmail.com>
2023-05-24 01:36:29 +0200

    added swap.py file with swapping function

---
+++ swap.py
@@ -0,0 +1,15 @@
+def swapping(x,y):
+ 
+    try:
+        if x>3 or x<0:
+            return "error number must be between 0 - 3"
+        if y>3 or y<0:
+            return "error number must be between 0 - 3"
+        
+        
+        a,b = y,x
+        return a,b
+    except Exception as e:
+        print(str(e))
+        
+        
